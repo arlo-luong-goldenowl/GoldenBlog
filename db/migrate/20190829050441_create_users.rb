78387a0ad092823321802b3f61bfaf0331dbe608
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-08-29 12:20:08 +0700

    added migration files and models, run 'rails db:migrate' or 'rails db:migrate:reset'( if want to delete and recreate database)

---
+++ db/migrate/20190829050441_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.text :image
+      t.string :password_digest
+      t.string :remember_digest
+      t.string :role
+      t.string :activation_digest
+      t.boolean :activated
+      t.date_time :activated_at
+      t.string :reset_digest
+
+      t.timestamps
+    end
+  end
+end
