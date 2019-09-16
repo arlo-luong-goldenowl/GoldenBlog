7485a2b2db671a965d3086a7ed50fb2b55de5549
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-09-16 16:32:41 +0700

    for testing-1

--- app/workers/mail_notification_worker.rb
+++ app/workers/mail_notification_worker.rb
@@ -3,21 +3,26 @@ class MailNotificationWorker
   sidekiq_options retry: false
 
   def perform(type, *argu)
-    users = User.all
-    users.each do |user|
-      if type == "new"
-        post_id = argu[0]
-        post = Post.find(post_id)
-        PostMailer.post_released_email(user, post).deliver
-      elsif type == "update"
-        post_id = argu[0]
-        post = Post.find(post_id)
-        PostMailer.post_edited_email(user, post).deliver
-      else
-        title = argu[0]
-        author = argu[1]
-        PostMailer.post_deleted_email(user, title, author).deliver
-      end
-    end
+    puts "TYPE"
+    puts type
+
+    puts "ARGU"
+    puts argu
+    # users = User.all
+    # users.each do |user|
+    #   if type == "new"
+    #     post_id = argu[0]
+    #     post = Post.find(post_id)
+    #     PostMailer.post_released_email(user, post).deliver
+    #   elsif type == "update"
+    #     post_id = argu[0]
+    #     post = Post.find(post_id)
+    #     PostMailer.post_edited_email(user, post).deliver
+    #   else
+    #     title = argu[0]
+    #     author = argu[1]
+    #     PostMailer.post_deleted_email(user, title, author).deliver
+    #   end
+    # end
   end
 end
