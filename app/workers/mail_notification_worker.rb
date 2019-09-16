23f52638733fff9631bbc9959ffd64cdf1991561
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-09-16 22:24:54 +0700

    22 24

--- app/workers/mail_notification_worker.rb
+++ app/workers/mail_notification_worker.rb
@@ -6,17 +6,20 @@ class MailNotificationWorker
     users = User.all
     users.each do |user|
       if type == "new"
-        post_id = argu[0]
-        post = Post.find(post_id)
-        PostMailer.post_released_email(user, post).deliver
+        puts "NEW"
+        # post_id = argu[0]
+        # post = Post.find(post_id)
+        # PostMailer.post_released_email(user, post).deliver
       elsif type == "update"
-        post_id = argu[0]
-        post = Post.find(post_id)
-        PostMailer.post_edited_email(user, post).deliver
+        puts "UPDATE"
+        # post_id = argu[0]
+        # post = Post.find(post_id)
+        # PostMailer.post_edited_email(user, post).deliver
       else
-        title = argu[0]
-        author = argu[1]
-        PostMailer.post_deleted_email(user, title, author).deliver
+        puts "DELETE"
+        # title = argu[0]
+        # author = argu[1]
+        # PostMailer.post_deleted_email(user, title, author).deliver
       end
     end
   end
