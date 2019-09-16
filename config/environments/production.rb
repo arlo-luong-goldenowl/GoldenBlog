21d69189ac1b1bca785c915462d169fcbc2ef153
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-09-16 17:49:48 +0700

    testing 17:50

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { :host => ENV['WEB_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: ENV['MAILGUN_SMTP_PORT'],
